bc30d5bd75657f3a43e936c3ae172573220b9090
Myouuu <meriem.mrabent@hetic.net>
2020-11-09 02:06:28 +0100

    Add Docker :whale:

--- src/Projection/UserProjector.php
+++ src/Projection/UserProjector.php
@@ -19,7 +19,7 @@ class UserProjector
  
     public function onUserRegistered(UserRegistered $userRegistered): void
     {
-        $query = $this->PDO->prepare('INSERT INTO `read_users` SET email = ?, password = ?, id = ?');
+        $query = $this->PDO->prepare('INSERT INTO `users` SET email = ?, password = ?, id = ?');
         $query->bindValue(1, $userRegistered->email());
         $query->bindValue(2, $userRegistered->password());
         $query->bindValue(3, $userRegistered->aggregateId());
@@ -29,7 +29,7 @@ class UserProjector
  
     public function onEmailChanged(EmailChanged $emailChanged): void
     {
-        $query = $this->PDO->prepare('UPDATE `read_users` SET email = ? WHERE id = ?');
+        $query = $this->PDO->prepare('UPDATE `users` SET email = ? WHERE id = ?');
         $query->bindValue(1, $emailChanged->email());
         $query->bindValue(2, $emailChanged->aggregateId());
         $query->execute();
